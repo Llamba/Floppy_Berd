b97b87cce50f683bb4f5dd48b3bba829cc3e9c69
Llamba <marvelcalvero@gmailc.om>
2022-10-28 20:54:40 -0400

    idk

--- pygame/flappy_bird.py
+++ pygame/flappy_bird.py
@@ -15,27 +15,50 @@ class Bird(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
         self.vel = 0
+        self.clicked = False
+        self.jumpforce = 10
+        self.start = False
+        self.gameover = False
 
     def update(self):
 
         #  gravity
-        self.vel += 0.5
-        if self.vel >= 15:
-            self.vel = 15
-        self.rect.y += self.vel
-        if self.rect.y >= 860:
-            self.vel = 0
+        if not self.gameover:
+            if pygame.mouse.get_pressed()[0] == 1:
+                self.start = True
+            if self.start:
+                self.vel += 0.5
+                if self.vel >= 15:
+                    self.vel = 15
+                self.rect.y += self.vel
+                if self.rect.y >= 900:
+                    self.gameover = True
 
-        #if pygame.mouse.get_pressed()[]:
-            #pass
+            #  Jump
+            if pygame.mouse.get_pressed()[0] == 1 and not self.clicked:
+                self.clicked = True
+                if self.vel >= -15:
+                    if self.vel > 0:
+                        self.vel = 0
+                        self.vel -= 10
+                    elif self.vel <= 0:
+                        self.vel -= 10
+                elif self.vel < 15:
+                    self.vel = -15
+            if pygame.mouse.get_pressed()[0] == 0:
+                self.clicked = False
 
             # animation
-        self.counter += 1
-        flap_cooldown = 5
-        if self.counter > flap_cooldown:
-            self.counter = 0
-            self.index += 1
-            if self.index >= len(self.images):
-                self.index = 0
+            self.counter += 1
+            flap_cooldown = 5
+            if self.counter > flap_cooldown:
+                self.counter = 0
+                self.index += 1
+                if self.index >= len(self.images):
+                    self.index = 0
 
-        self.image = self.images[self.index]
+        #  Rotation
+        if not self.gameover:
+            self.image = pygame.transform.rotate(self.images[self.index], self.vel * -1)
+        if self.gameover:
+            self.image = pygame.transform.rotate(pygame.image.load("img/deadbird.png"), 90)
--- pygame/learningpygame.py
+++ pygame/learningpygame.py
@@ -1,4 +1,5 @@
 import pygame
+from flappy_bird import Bird
 
 pygame.init()
 
@@ -17,9 +18,8 @@ trees_img = pygame.image.load("img/whatever these are.png")
 # Game variables
 ground_scroll = 0
 scroll_speed = 2
-
-from flappy_bird import Bird
-
+flying = False
+game_over = False
 
 berd_group = pygame.sprite.Group()
 
@@ -31,11 +31,16 @@ while running:
 
     clock.tick(fps)
 
+    if berd.rect.y < 900:
+        game_over = True
     screen.blit(bg, (0, 0))
-    screen.blit(trees_img, (ground_scroll, 584))
+    screen.blit(trees_img, (ground_scroll, 610))
+    if game_over:
+        ground_scroll -= scroll_speed
+
     berd_group.draw(screen)
-    ground_scroll -= scroll_speed
     berd_group.update()
+
     if ground_scroll == -1920:
         ground_scroll = 0
     for event in pygame.event.get():
